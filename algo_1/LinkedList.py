b3aed21b0f8a02ba0b0f34d46ed783c5eb797a5f
rohan-winsor <44546060+rohan-winsor@users.noreply.github.com>
2021-04-03 11:20:18 +0530

    Update LinkedList.py

--- algo_1/LinkedList.py
+++ algo_1/LinkedList.py
@@ -2,6 +2,7 @@
 My interpretation of linkedlist in z2m 
 """
 
+
 class LinkedList:
     def __init__(self, value):
         self.head = {
@@ -23,17 +24,47 @@ class LinkedList:
                 self.append(app_value, item["next"])
         return None
 
+    def prepend(self, value):
+        self.head = { "value" : value , "next" : self.head}
+
     def __repr__(self):
         return str(self.head)
 
 
+"""Better Linked List
+"""
+
+
+class Nodee:
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
+
+
+class LinkedList2:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, value):
+        self.head = Nodee(value, self.head)
+
+    def __repr__(self):
+        linkedlist = self.head
+        out = []
+        while linkedlist.next is not None:
+            out.append(str(linkedlist.data))
+            linkedlist = linkedlist.next
+        out.append(str(linkedlist.data))
+        return " --> ".join(out)
+
+
 linkded_list = LinkedList(2)
-linkded_list.append(5)
-linkded_list.append(6)
-linkded_list.append(5)
-linkded_list.append(6)
-linkded_list.append(5)
-linkded_list.append(6)
-linkded_list.append(5)
-linkded_list.append(6)
+linkded_list.prepend(5)
+linkded_list.prepend(6)
+linkded_list.prepend(7)
+linkded_list.prepend(8)
+linkded_list.prepend(9)
+linkded_list.prepend(10)
+linkded_list.prepend(11)
+linkded_list.prepend(12)
 print(linkded_list)
